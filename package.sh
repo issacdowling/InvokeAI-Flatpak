48d3bd13d772c2ad0c398afb77453f07a29fc022
issacdowling <git@issacdowling.com>
2025-05-07 21:13:04 +0100

    Explain installation more, remove unnecessary flatpak build steps

--- package.sh
+++ package.sh
@@ -13,7 +13,7 @@ sed -i 's/Exec=AppRun --no-sandbox %U/Exec=\/app\/AppRun %U/g' ./squashfs-root/c
 
 flatpak run org.flatpak.Builder --user --install --force-clean build-dir com.invoke.community.json
 
-echo "Before running the installer, you'll need to allow internet access (as it's disabled by default).\nYou can either do this using Flatseal or by running:\nflatpak override --user --share=network com.invoke.community\nthen running the same thing but with --unshare instead of --share to remove it later."
+echo "Before running the installer, you'll need to allow internet access (as it's disabled by default).\nYou can either do this using Flatseal or by running:\nflatpak override --user --share=network com.invoke.community\nthen running the same thing but with --unshare instead of --share to remove it once installed.\nSet the install directory to ~/.var/app/com.invoke.community/data/install"
 
 # Icon things are very broken and will not export for me, so I'm just manually putting it where it needs to go (the flatpak exports directory doesn't make it happy), which is bad but works
 cp ./build-dir/files/usr/share/icons/hicolor/1024x1024/apps/invoke-community-edition.png ~/.local/share/icons/com.invoke.community.png
